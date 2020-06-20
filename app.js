60d07d3d5989b1b9b7615850cd54dac46e0b316f
Adem24 <adem.benzarb@esprit.tn>
2020-06-20 18:07:33 +0100

    fixing cors policy

--- app.js
+++ app.js
@@ -21,7 +21,23 @@ var db = require('./models/db')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors())
+app.use((req, res, next) => 
+{
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
